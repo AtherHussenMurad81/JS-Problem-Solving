c6e7099f5006db68148c891069136eb4420f0d13
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-02-27 16:16:33 +0600

    problem solving

---
+++ script.js
@@ -0,0 +1,38 @@
+const num = "20";
+const convertNum = parseInt(num);
+// console.log(convertNum + 5);
+const floatNum = "3.12159";
+
+const float = parseInt(floatNum);
+// console.log(float);
+
+let me = 7;
+let bro = 5;
+
+// if (me > bro) {
+//   console.log("আমি বেশি কেয়েছি");
+// } else {
+//   console.log("আমার ভাই বেশি কেয়েছি");
+// }
+
+const myNum = 45;
+const frndNum = 97;
+
+// if (myNum > frndNum) {
+//   console.log("I much nmbr from frnd");
+// } else {
+//   console.log("My frnd much number");
+// }
+// console.log("javaScripts" == "javaScripts");
+// console.log("javaScripts" === "javaScripts");
+//   console.log(10 !== "10" );
+// console.log(10 != "10");
+// console.log("t" == "T");
+// console.log("T" == "T");
+
+const vX = 15;
+
+const vY = "15";
+
+console.log(vX != vY, "1");
+console.log(vX !== vY);
