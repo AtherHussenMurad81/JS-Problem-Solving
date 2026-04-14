2d9df5e44721c6b3cfde06399849a4322bbe3b5b
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-04-14 20:51:38 +0600

    donsin

---
+++ js-function/return.js
@@ -0,0 +1,52 @@
+// function tenTimes(number) {
+//   const result = number * 10;
+//   return result;
+// }
+
+// const output = tenTimes(5);
+
+// console.log("output");
+
+// const potato = 35;
+
+// const onioin = 40;
+
+// const shirt = 500;
+
+// const pant = 1000;
+
+// function add(pr1, pr2) {
+//   return pr1 + pr2;
+// }
+// const finalBut = { potato, onioin, shirt, pant };
+
+// const bill = add(onioin, shirt);
+// const bill2 = add(potato, pant);
+// console.log(bill + bill2);
+// console.log(finalBut);
+// ..................... HW............
+// 1
+// function less10(a) {
+//   if (a <= 10) return true;
+//   else return false;
+// }
+// console.log(less10(5));
+
+// 2
+// function isDivition13(a) {
+//   if (a % 13 === 0) return true;
+//   else return false;
+// }
+// // const isDivition13 = 26;
+// console.log(isDivition13(39));
+
+// 7
+function isNagative(a) {
+  if (a < 0) {
+    return a * -1;
+  } else {
+    return false || "gives me nagative value";
+  }
+}
+
+console.log(isNagative(4));
