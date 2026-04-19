4d398498a8d55aa59acac001a5912582fc856f74
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-04-19 10:10:58 +0600

    function created

---
+++ js-problem-solving-part-1/height.js
@@ -0,0 +1,60 @@
+// function inchFeet(inch) {
+//   const feet = inch / 12;
+//   return feet;
+// }
+
+// const muradHeight = inchFeet(75);
+// console.log(muradHeight);
+
+// function calHeight(inchi) {
+//   const feet = inchi / 12;
+
+//   const remainingInchi = inchi % 12;
+//   const result = feet + " Fit " + remainingInchi + " remainingInchi";
+//   return result;
+// }
+// const nahidaHeight = 75;
+
+// console.log(calHeight(nahidaHeight));
+
+// function mileToKelo(mile) {
+//   const kilo = mile * 1.60934;
+//   return kilo;
+// }
+
+// 1
+// function calGoj(goj) {
+//   const totalGoj = 1760 * goj;
+//   return totalGoj;
+// }
+// console.log(calGoj(2));
+
+// 2
+// function keliWord(kelo) {
+//   const result = 860 * kelo;
+//   return result;
+// }
+// console.log(keliWord(4));
+
+// 3
+// function calSecond(hour) {
+//   const minit = hour * 60;
+//   const second = minit * 60;
+//   const result = hour + " hour is equal to " + second + " second";
+//   return result;
+// }
+// console.log(calSecond(2));
+
+// 3
+// function convertMiter(sent) {
+//   const miter = sent / 100;
+//   return miter;
+// }
+
+// console.log(convertMiter(200));
+
+// 5
+// function convertSentimeter(ichi) {
+//   const sentimeter = ichi * 2.54;
+//   return sentimeter;
+// }
---
+++ js-problem-solving-part-1/lip-year.js
@@ -0,0 +1,21 @@
+// function isLeapYear(year) {
+//   if (year % 400 === 0) {
+//     return true;
+//   } else if (year % 100 === 0) {
+//     return false;
+//   } else if (year % 4 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// const isLipi1 = isLeapYear(2100);
+// const isLipi2 = isLeapYear(2400);
+// const isLipi3 = isLeapYear(1900);
+// const isLipi4 = isLeapYear(2052);
+
+// console.log(isLipi1, isLipi2, isLipi3, isLipi4);
+
+// Output: false true false true
+
+
