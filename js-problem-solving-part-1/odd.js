e7badae6d8474c12b6845e1c33316c0170d1e047
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-04-23 19:22:09 +0600

    noDuplicate number

--- js-problem-solving-part-1/odd.js
+++ js-problem-solving-part-1/odd.js
@@ -65,24 +65,99 @@
 
 // 1
 
-function evenNumbers(numbers) {
-  const evenNums = [];
+// function evenNumbers(numbers) {
+//   const evenNums = [];
 
-  for (const num of numbers) {
-    if (num % 2 === 0) {
-      evenNums.push(num);
-    }
-  }
+//   for (const num of numbers) {
+//     if (num % 2 === 0) {
+//       evenNums.push(num);
+//     }
+//   }
 
-  let sum = 0;
+//   let sum = 0;
 
-  for (const even of evenNums) {
-    sum += even;
-  }
-  const count = evenNums.length;
-  const average = sum / count;
-  return average;
-}
+//   for (const even of evenNums) {
+//     sum += even;
+//   }
+//   const count = evenNums.length;
+//   const average = sum / count;
+//   return average;
+// }
 
-const num = [2, 3, 4, 5, 6];
-console.log(evenNumbers(num));
+// const num = [1, 2, 3, 4, 5, 6];
+// console.log(evenNumbers(num));
+
+// 2
+
+// function oddNumbers(numbers) {
+//   const oddNums = [];
+//   for (const num of numbers) {
+//     if (num % 2 === 1) {
+//       oddNums.push(num);
+//     }
+//   }
+//   console.log(oddNums);
+//   const newArray = [];
+//   for (const mult of oddNums) {
+//     const result = mult * 2;
+//     // result.push(newArray);
+//     newArray.push(result);
+//   }
+
+//   console.log(newArray);
+// }
+// const num = [4, 5, 67, 7, 3, 12];
+
+// 3
+
+// function oddNumbers(numbers) {
+//   for (const odd of numbers) {
+//     if (numbers % 2 === 1) {
+//       return "Odd Numbers found";
+//     }
+//   }
+//   return "No Odd numbers found";
+// }
+// console.log(oddNumbers([4, 5, 8]));
+
+// or
+
+// function oddNumbers(numbers) {
+//   for (const num of numbers) {
+//     if (num % 2 === 1) {
+//       return "Odd numbers found";
+//     }
+//   }
+//   return "No odd numbers found";
+// }
+
+// const num = [4, 6, 8, 10];
+// console.log(oddNumbers(num)); // No odd numbers found
+
+// const num2 = [4, 5, 6];
+// console.log(oddNumbers(num2)); // Odd numbers found
+
+// function plus1Odd(numbers) {
+//   let oddNumbers = [];
+
+//   for (const odd of numbers) {
+//     if (odd % 2 === 1) {
+//       oddNumbers.push(odd);
+//     }
+//   }
+//   //   console.log(oddNumbers);
+//   let newArray = [];
+//   for (const plus1 of oddNumbers) {
+//     const plusOne = plus1 + 1;
+//     // return plusOne;
+//     // console.log(plusOne);
+//     newArray.push(plusOne);
+//   }
+//   //   return newArray;
+
+//   const sortArray = newArray.sort((a, b) => b - a);
+//   return sortArray;
+// }
+// const num2 = [4, 9, 6, 11];
+
+// console.log(plus1Odd(num2));
---
+++ js-problem-solving-part-1/tuins-biriyani.js
@@ -0,0 +1,27 @@
+// const students = ["abul", "babul", "cabul", "abul", "babul", "dabul"];
+// const numbers = [1, 5, 61, 5, 87, 7, 5, 81, 61];
+
+// function noDuplicate(array) {
+//   const unique = [];
+//   for (const item of array) {
+//     if (unique.includes(item) === false) {
+//       unique.push(item);
+//     }
+//   }
+//   return unique;
+// }
+// const uniqueArray = noDuplicate(students);
+// console.log(uniqueArray);
+
+// const array = [1, 2, 5, 61, 5, 87, 7, 5, 81, 71, 71];
+
+// const noDup = (arr) => {
+//   const uni = [];
+//   for (const item of arr) {
+//     if (uni.includes(item) === false) {
+//       uni.push(item);
+//     }
+//   }
+//   return uni;
+// };
+// console.log(noDup(array));
